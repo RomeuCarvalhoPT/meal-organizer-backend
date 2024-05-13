178a8d631ea328ba0422d2ff7487556401b1e279
RomeuCarvalhoPT <romeublue@gmail.com>
2024-05-13 14:16:16 +0000

    added endpoint to store files

---
+++ routes/files.js
@@ -0,0 +1,37 @@
+//routes/files.js
+const express = require("express");
+const router = express.Router();
+const upload = require("../middleware/upload");
+
+// Route for uploading files
+router.post("/upload", upload.single("image"), (req, res) => {
+  try {
+    // Respond with the URL of the uploaded file
+    const host = req.get('host');
+    const filePath = "https://" + host + "/files/" + req.file.filename;
+
+    return res
+      .status(200)
+      .json({
+        message: "File uploaded successfully!",
+        url: filePath,
+      });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
+const path = require('path');
+
+
+router.get("/:filename", async (req, res) => {
+  const filename = req.params.filename;
+  const file = path.join(__dirname, '../uploads', filename);
+  res.sendFile(file);
+});
+
+router.use("/files", express.static(path.join(__dirname, 'uploads')));
+
+//router.use("/files", express.static("./uploads/"));
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,22 +3,33 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./database');
 const dishRoutes = require('./routes/dishes');
+const filesRoutes = require('./routes/files');
 const ingredientRoutes = require('./routes/ingredients');
 const { Dish, Ingredient, DishIngredient } = require('./models');
+
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+
 // Sync models with the database
 sequelize.sync() // Use { force: true } for development to drop tables and recreate them
  .then(() => console.log('Database & tables created!'))
  .catch(err => console.error('Error syncing models:', err));
 
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
 // Use your routes
 app.use('/dishes', dishRoutes);
 app.use('/ingredients', ingredientRoutes);
+app.use('/files', filesRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
